4a395d4a71175a3bdd39841b90ca9ec05338f30f
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-23 19:35:15 +0100

    Scaffolded all functions

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -1,13 +1,30 @@
-
 import './App.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <div>
+      <Logo/>
+      <Form/>
+      <List/>
+      <Stats/>
+    </div>
   )
 }
+
+function Logo(){
+  return <h1> Far Away</h1>
+}
+function Form(){
+  return <div> <h3>What do you need for your trip?</h3></div>
+}
+function List(){
+  return <div>LIST</div>
+}
+function Stats(){
+  return <footer> You have X items on your list. You packed already packed X items</footer>
+}
+
+
 
 export default App
