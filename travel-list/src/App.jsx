129d1f5b961609f873afe0c3ef7b2c2b84adcd0f
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-25 22:56:52 +0100

    searchbar

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -21,20 +21,27 @@ function Logo(){
 }
 function Form(){
   const [description,setDescription] = useState("")
+  const [quantity,setQuantity] = useState(5)
 
   function handleSubmit(event) {
     event.preventDefault();
   }
   return (
-    <form onSubmit={handleSubmit}>
+    <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your trip?</h3>
-      <select>
+      <select
+        value={quantity}
+        onChange={(e)=> setQuantity(e.target.value)}>
         {Array.from({leength:20}, (_,i) => i+1).map(num=>
           <option value={num} key={num}>
             {num}
           </option>)}
       </select>
-      <input type="text" placeholder="Item..." value={description} onChange={(e)=> setDescription(e.target.value)}/>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e)=> setDescription(e.target.value)}/>
       <button>Add</button>
     </form>
   )
