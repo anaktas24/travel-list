6ecf017e1ee07a350b1ccffd920189bfc59e4024
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-30 20:53:09 +0100

    handle items

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -3,50 +3,69 @@ import {useState} from 'react'
 
 
 
-const initialItems = [
-  { id:1, description: "Pass", quantity:2, packed: false}]
+const initialItems = [{ id:1, description: "Pass", quantity:2, packed: false}]
 
 function App() {
 
-  return (
-    <div>
-      <Logo/>
-      <Form/>
-      <List/>
-      <Stats/>
-    </div>
-  )
+return (
+  <div>
+    <Logo/>
+    <Form/>
+    <List/>
+    <Stats/>
+  </div>
+)
 }
 
 function Logo(){
   return <h1> Far Away</h1>
 }
-function Form(){
-  const [description,setDescription] = useState("")
-  const [quantity,setQuantity] = useState(5)
 
-  function handleSubmit(event) {
-    event.preventDefault();
+
+function Form(){
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+  const [items, setItems] = useState([])
+
+  function handleAddItems(item){
+    setItems(items=>[...items, item])
   }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    handleAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
     <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do you need for your trip?</h3>
+      <h3>What do you need for your 😍 trip?</h3>
       <select
         value={quantity}
-        onChange={(e)=> setQuantity(e.target.value)}>
-        {Array.from({leength:20}, (_,i) => i+1).map(num=>
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
-          </option>)}
+          </option>
+        ))}
       </select>
       <input
         type="text"
         placeholder="Item..."
         value={description}
-        onChange={(e)=> setDescription(e.target.value)}/>
+        onChange={(e) => setDescription(e.target.value)}
+      />
       <button>Add</button>
     </form>
-  )
+  );
 }
 function List(){
   return (
@@ -73,7 +92,5 @@ function Item({item}){
 function Stats(){
   return <footer className='stats'> You have X items on your list. You packed already packed X items</footer>
 }
-
-
 
 export default App
