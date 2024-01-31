f96a050eb9d4817142a85d923b3be12630020d7e
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-31 17:41:49 +0100

    css

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -9,9 +9,7 @@ function App() {
   const [items, setItems] = useState([])
 
 
-  function handleAddItems(item){
-    setItems(items=>[...items, item])
-  }
+
 
   function handleDeleteItem(id){
     setItems((items)=> items.filter((item=>item.id!==id)))
