dbaeeb5fa1a879fb3722f4b89b128f66b195e481
Ioannis Antoniou <johnantony24@hotmail.com>
2024-01-31 17:42:06 +0100

    function delete fixed

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -9,7 +9,9 @@ function App() {
   const [items, setItems] = useState([])
 
 
-
+  function handleAddItems(item){
+    setItems(items=>[...items, item])
+  }
 
   function handleDeleteItem(id){
     setItems((items)=> items.filter((item=>item.id!==id)))
