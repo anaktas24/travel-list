a6a952c3cc90cb04ea1e890cbb2c3097978b20ff
Ioannis Antoniou <johnantony24@hotmail.com>
2024-02-02 19:03:32 +0100

    Added conditional if array===empty

--- travel-list/src/App.jsx
+++ travel-list/src/App.jsx
@@ -106,9 +106,25 @@ function Item({item, onDeleteItem, onToggleItem}){
   )
 }
 function Stats({items}){
+  if (!items.length)
+   return
+    <p className='footer'>
+      <em>
+        Start adding items to your list
+      </em>
+    </p>
   const numItems = items.length
   const numPacked = items.filter(item=> item.packed).length
-  return <footer className='stats'> You have X items on your list. You packed already packed X items</footer>
+  const percentage = Math.round((numPacked/numitems)* 100)
+  return (
+    <footer className='stats'>
+      <em>
+        {percentage===100 ? 'You got everything packed!' :
+        `You have ${numItems} items on your list.
+        You packed already packed ${numPacked} (${percentage}%)items`}
+      </em>
+    </footer>
+  )
 }
 
 export default App
